b8a0153c4b6450db3e07aaab2c77651cc747a7d4
izima01 <kingsleyizima@gmail.com>
2022-11-26 18:54:05 +0100

    fixed the pesky errors

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const detailsContainer = document.querySelector("#details-container");
 let countryData = [];
 
 const themeButton = document.querySelector("header button");
-console.log(themeButton.firstElementChild);
 themeButton.addEventListener('click', ()=> {
     console.log("Change theme");
     document.body.classList.toggle("darkmode");
@@ -88,100 +87,16 @@ const searchRegion = () => {
         });
     }
 }
+
 // countrySection.parentElement
 countrySection.addEventListener('click', (event) => {
-    console.log(detailsContainer.classList);
     let target;
     if ((event.target.tagName === "H3") || (event.target.tagName === "P") || (event.target.className === "card-body") || (event.target.tagName === "IMG")) {
         target = event.target.parentElement.firstElementChild.innerHTML;
-        detailsContainer.classList.remove("d-none");
-        countrySection.classList.add("d-none");
+        console.log({ target });
+        sessionStorage.setItem('name', target);
+        window.location.href = 'details.html';
     };
-    // console.log(target);
-    getSingleData(target);
-});
-
-document.querySelector(".back").addEventListener('click', () => {
-    detailsContainer.classList.add("d-none");
-    countrySection.classList.remove("d-none");
 });
 
 // Details page script
-let countryInfo;
-const templateBtn = document.getElementById("sampleBtn").content;
-
-function getSingleData(name) {
-    let singleUrl = `https://restcountries.com/v3.1/name/${name}?fullText=true`;
-    fetch(singleUrl)
-    .then(response => response.json())
-    .then(data => {
-        countryInfo = data[0];
-        renderDetails();
-    });
-};
-
-const getBorderInfo = (array) => {
-    detailsContainer.querySelector(".border-countries div").innerHTML = "";
-    array?.forEach(country => {
-        let singleUrl = `https://restcountries.com/v3.1/alpha/${country}`;
-        fetch(singleUrl)
-        .then(response => response.json())
-        .then(data => {
-            let templateCopy = document.importNode(templateBtn, true);
-            console.log(data[0].name.common);
-            templateCopy.querySelector(".border").textContent = data[0].name.common;
-            detailsContainer.querySelector(".border-countries div").appendChild(templateCopy);
-        });
-    });
-};
-
-detailsContainer.querySelector(".border-countries div").addEventListener('click', (event)=> {
-    if (event.target.className === "border") {
-        console.log("Thats a border");
-        // console.log(event.target.innerHTML);
-        getSingleData(event.target.innerHTML);
-    }
-});
-
-
-let flag = detailsContainer.querySelector("#flagImg");
-let fullName = detailsContainer.querySelector(".fullName");
-let nativeName = detailsContainer.querySelector(".native-name");
-let population = detailsContainer.querySelector(".population");
-let region = detailsContainer.querySelector(".region");
-let subRegion = detailsContainer.querySelector(".sub-region");
-let capital = detailsContainer.querySelector(".capital");
-let domain = detailsContainer.querySelector(".domain");
-let currencies = detailsContainer.querySelector(".Currencies");
-let lang = detailsContainer.querySelector(".lang");
-
-const renderDetails = () => {
-    console.log(countryInfo);
-    flag.setAttribute("src", countryInfo?.flags?.png);
-    fullName.innerHTML = countryInfo?.name?.official || " ";
-    nativeName.innerHTML = countryInfo?.name?.nativeName?.eng?.official || countryInfo?.name?.official || " ";
-    population.innerHTML = countryInfo.population;
-    region.innerHTML = countryInfo.region || " ";
-    subRegion.innerHTML = countryInfo.subregion || " ";
-    capital.innerHTML = countryInfo.capital || " ";
-    domain.innerHTML = countryInfo.tld[0] || " ";
-    currencies.innerHTML = countryInfo.currencies ? Object.keys(countryInfo.currencies)[0] : " ";
-    let languages = countryInfo.languages ? Object.values(countryInfo.languages): " ";
-    // console.log(languages);
-    let langList = languages && languages.join(", ");
-    // console.log(langList);
-    lang.innerHTML = langList;
-    let borderArray = countryInfo?.borders;
-    console.log(borderArray);
-    if (!borderArray) {
-        console.log("no borders");
-        detailsContainer.querySelector(".border-countries div").innerHTML = "No borders"
-    } else {
-        getBorderInfo(borderArray)
-    }
-    // (!borderArray) ? detailsContainer.querySelector(".border-countries div").innerHTML = "No borders" : getBorderInfo(borderArray);
-}
-
-let x = 12908376;
-let numString = String(x).replace(/(.)(?=(\d{3})+$)/g, '$1,')
-console.log(x);
